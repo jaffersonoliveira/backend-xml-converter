9d9577b48ce7af24fde4001df1d2ef17ee7dce2c
Jafferson Oliveira <jafferson.oliveira@grupoadolfolutz.com.br>
2023-07-04 18:09:10 -0300

    feat: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,28 @@
-import express, { Errback, NextFunction, Request, Response } from 'express';
 import { config as dotenv } from 'dotenv';
-import helmet from 'helmet'
+import express, { Errback, NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
 
 const server = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
-dotenv({path: process.env.NODE_ENV === 'production' ? '.env' : '.env.example'});
+dotenv({
+  path: process.env.NODE_ENV === 'production' ? '.env' : '.env.example',
+});
 
 server.use(helmet());
 server.use(express.json());
 
-server.use((error: Errback, req: Request, res: Response, next: NextFunction): any =>{
-    console.log(new Date().toISOString(), " - ", error);
-    res.status(500).send("Internal Server Error");
-});
+server.use(
+  (error: Errback, req: Request, res: Response, next: NextFunction) => {
+    console.log(new Date().toISOString(), ' - ', error);
+    res.status(500).send('Internal Server Error');
+  },
+);
 
-server.listen(port, ()=>console.log(new Date().toISOString(), ` - ⚡️ Server is running at ${host}:${port}`));
+server.listen(port, () =>
+  console.log(
+    new Date().toISOString(),
+    ` - ⚡️ Server is running at ${host}:${port}`,
+  ),
+);
